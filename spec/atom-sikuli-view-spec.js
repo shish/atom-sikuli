2eeaab746399150190b560a55ae378f6bf7e544a
Shish <shish@shishnet.org>
2017-08-24 11:49:25 +0100

    Initial import - inline images and start of a control panel

---
+++ spec/atom-sikuli-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomSikuliView from '../lib/atom-sikuli-view';
+
+describe('AtomSikuliView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
