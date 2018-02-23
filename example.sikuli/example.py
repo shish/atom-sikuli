14ac7c28b73591a84e355f9d7758660635c2db82
Shish <shish@shishnet.org>
2018-02-23 11:16:54 +0000

    more

--- example.sikuli/example.py
+++ example.sikuli/example.py
@@ -4,3 +4,6 @@ from sikuli import *
 s = Screen()
 match = s.find("1504123790458.png")
 print("Found MagicPython at " + match)
+
+
+s.click(Pattern("1504169483581.png").similarity(0.4).offset(5, 5))
